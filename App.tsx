8b79c67a6db484b6baa9988634134ebe55938611
Deckerds <tjtharuka5@gmail.com>
2024-03-14 09:20:33 +0530

    basic text and inputs

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Tharuka');
+  const [age, setAge] = useState('28');
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter Name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder='e.g. John Doe'
+        onChangeText={(val) => setName(val)}
+      />
+      <Text>Enter Age:</Text>
+      <TextInput
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 30'
+        onChangeText={(val) => setAge(val)}
+      />
+      <Text>
+        Name: {name}, Age: {age}
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -16,5 +35,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
